440dadb76c1a6d4c7b0c396f6cb6e311d4bc683d
musong <txz32102@gmail.com>
2024-07-25 15:30:56 +0800

    add link for sidebar page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView, useRoute } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
+
+const route = useRoute();
 </script>
 
 <template>
-  <header>
+  <header v-if="route.path !== '/main'">
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -13,6 +15,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/main">Main</RouterLink>
       </nav>
     </div>
   </header>
